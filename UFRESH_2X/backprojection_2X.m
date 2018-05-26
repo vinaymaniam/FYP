0837684d37ea92608d83cb5b8fce971d0c6674f6
vm1414 <vm1414@ic.ac.uk>
2018-05-26 15:17:05 +0100

    everything good, just need to collect data and do final task

--- UFRESH_2X/backprojection_2X.m
+++ UFRESH_2X/backprojection_2X.m
@@ -1,11 +1,11 @@
-function I_bp=backprojection_2X(Ir,Orig,filter) 
-    % the LL part of the original FRESH image is actually our LR image
-    [c_org,l_org] = wavedec2(Orig,1,filter);
+function I_bp=backprojection_2X(SR,HR,filter) 
+    % the LL part of the HR image is actually our LR image
+    [c_org,l_org] = wavedec2(HR,1,filter);
     Ilowc = appcoef2(c_org,l_org,filter,1)./2;
     
     rangeImg=[min(Ilowc(:)) max(Ilowc(:))];
-    Ir=range0toN(Ir,rangeImg);
-    [c1,l1] = wavedec2(Ir,1,filter);
+    SR=range0toN(SR,rangeImg);
+    [c1,l1] = wavedec2(SR,1,filter);
     [h1,v1,d1] = detcoef2('all',c1,l1,1);
     
     c_rec=[2*Ilowc(:);  h1(:); v1(:); d1(:);];
